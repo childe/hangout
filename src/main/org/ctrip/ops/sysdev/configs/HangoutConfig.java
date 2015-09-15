a870b03b6cbbf6dd40ccd63c8d757bbc3fe2e701
childe <heychilde@gmail.com>
2015-09-15 11:10:35 +0800

    modified:   src/main/org/ctrip/ops/sysdev/configs/HangoutConfig.java

--- src/main/org/ctrip/ops/sysdev/configs/HangoutConfig.java
+++ src/main/org/ctrip/ops/sysdev/configs/HangoutConfig.java
@@ -12,54 +12,15 @@ import org.ctrip.ops.sysdev.Main;
 import org.yaml.snakeyaml.Yaml;
 
 public class HangoutConfig {
-	private static final Logger logger = Logger.getLogger(HangoutConfig.class.getName());
+	private static final Logger logger = Logger.getLogger(HangoutConfig.class
+			.getName());
 
-	public static Map parse(String filename) {
+	public static Map parse(String filename) throws FileNotFoundException {
 		Yaml yaml = new Yaml();
 		FileInputStream input;
-		try {
-			input = new FileInputStream(new File(filename));
-			Map configs = (Map) yaml.load(input);
-			return configs;
-		} catch (FileNotFoundException e) {
-			logger.error(String.format("parse %s failed", filename));
-			logger.trace(e.getMessage());
-			return null;
-		}
-
-		// final HashMap kafkaInput1 = new HashMap();
-		// kafkaInput1.put("topic", "metric_thrift.15s");
-		// kafkaInput1.put("groupID", "hangout");
-		// kafkaInput1.put("zk", "192.168.81.156:2181");
-		//
-		// final HashMap kafkaInput2 = new HashMap();
-		// kafkaInput2.put("topic", "metric_thrift.1m");
-		// kafkaInput2.put("groupID", "hangout");
-		// kafkaInput2.put("zk", "192.168.81.156:2181");
-		//
-		// ArrayList<Map> inputs = new ArrayList<Map>() {
-		// {
-		// add(new HashMap() {
-		// {
-		// put("Kafka", kafkaInput1);
-		// }
-		// });
-		// add(new HashMap() {
-		// {
-		// put("Kafka", kafkaInput2);
-		// }
-		// });
-		// }
-		// };
-		// configs.put("inputs", inputs);
-
-		// return configs;
+		input = new FileInputStream(new File(filename));
+		Map configs = (Map) yaml.load(input);
+		return configs;
 
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
