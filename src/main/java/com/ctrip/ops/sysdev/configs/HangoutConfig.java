10e5b8d9f28acdf7782fae9bb04c87bd5bb8e83e
childe <heychilde@gmail.com>
2016-03-08 15:31:19 +0800

    intellij suggest

--- src/main/java/com/ctrip/ops/sysdev/configs/HangoutConfig.java
+++ src/main/java/com/ctrip/ops/sysdev/configs/HangoutConfig.java
@@ -2,20 +2,23 @@ package com.ctrip.ops.sysdev.configs;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
+@SuppressWarnings("rawtypes")
 public class HangoutConfig {
-//    private static final Logger logger = Logger.getLogger(HangoutConfig.class
-//            .getName());
+    private static final String HTTP = "http://";
+    private static final String HTTPS = "https://";
+//    private static final Logger logger = Logger.getLogger(HangoutConfig.class.getName());
 
-    public static Map parse(String filename) throws FileNotFoundException {
+    @SuppressWarnings("NestedMethodCall")
+    public static Map parse(String filename) throws IOException {
         Yaml yaml = new Yaml();
-        if (filename.startsWith("http://") || filename.startsWith("https://")) {
+        if (HangoutConfig.HTTP.startsWith(filename) || filename.startsWith(HangoutConfig.HTTPS)) {
             URL httpUrl;
             URLConnection connection;
             httpUrl = new URL(filename);
@@ -23,8 +26,7 @@ public class HangoutConfig {
             connection.connect();
             return (Map) yaml.load(connection.getInputStream());
         } else {
-            FileInputStream input;
-            input = new FileInputStream(new File(filename));
+            FileInputStream input = new FileInputStream(new File(filename));
             return (Map) yaml.load(input);
         }
     }
