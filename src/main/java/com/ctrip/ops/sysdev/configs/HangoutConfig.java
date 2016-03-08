9978ba44cb5d6db4192f1baf7428549d6c00e1d7
childe <heychilde@gmail.com>
2016-03-08 13:23:53 +0800

    http config

--- src/main/java/com/ctrip/ops/sysdev/configs/HangoutConfig.java
+++ src/main/java/com/ctrip/ops/sysdev/configs/HangoutConfig.java
@@ -3,21 +3,29 @@ package com.ctrip.ops.sysdev.configs;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
 import org.yaml.snakeyaml.Yaml;
 
 public class HangoutConfig {
-	private static final Logger logger = Logger.getLogger(HangoutConfig.class
-			.getName());
+//    private static final Logger logger = Logger.getLogger(HangoutConfig.class
+//            .getName());
 
-	public static Map parse(String filename) throws FileNotFoundException {
-		Yaml yaml = new Yaml();
-		FileInputStream input;
-		input = new FileInputStream(new File(filename));
-		Map configs = (Map) yaml.load(input);
-		return configs;
-
-	}
+    public static Map parse(String filename) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        if (filename.startsWith("http://") || filename.startsWith("https://")) {
+            URL httpUrl;
+            URLConnection connection;
+            httpUrl = new URL(filename);
+            connection = httpUrl.openConnection();
+            connection.connect();
+            return (Map) yaml.load(connection.getInputStream());
+        } else {
+            FileInputStream input;
+            input = new FileInputStream(new File(filename));
+            return (Map) yaml.load(input);
+        }
+    }
 }
