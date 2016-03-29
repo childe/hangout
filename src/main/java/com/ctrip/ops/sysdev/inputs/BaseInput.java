a22288d89b8522cb1d0e06ef6d2bac10379ba964
childe <heychilde@gmail.com>
2016-03-29 16:08:27 +0800

    log filter/output build process

--- src/main/java/com/ctrip/ops/sysdev/inputs/BaseInput.java
+++ src/main/java/com/ctrip/ops/sysdev/inputs/BaseInput.java
@@ -14,118 +14,124 @@ import com.ctrip.ops.sysdev.decoder.PlainDecoder;
 import com.ctrip.ops.sysdev.filters.BaseFilter;
 
 public abstract class BaseInput {
-	private static final Logger logger = Logger.getLogger(BaseInput.class
-			.getName());
+    private static final Logger logger = Logger.getLogger(BaseInput.class
+            .getName());
 
-	protected Map<String, Object> config;
-	protected IDecode decoder;
-	protected BaseFilter[] filterProcessors;
-	protected BaseOutput[] outputProcessors;
-	protected ArrayList<Map> filters;
-	protected ArrayList<Map> outputs;
+    protected Map<String, Object> config;
+    protected IDecode decoder;
+    protected BaseFilter[] filterProcessors;
+    protected BaseOutput[] outputProcessors;
+    protected ArrayList<Map> filters;
+    protected ArrayList<Map> outputs;
 
-	public BaseFilter[] createFilterProcessors() {
-		if (filters != null) {
-			filterProcessors = new BaseFilter[filters.size()];
+    public BaseFilter[] createFilterProcessors() {
+        if (filters != null) {
+            filterProcessors = new BaseFilter[filters.size()];
 
-			int idx = 0;
-			for (Map filter : filters) {
-				Iterator<Entry<String, Map>> filterIT = filter.entrySet()
-						.iterator();
+            int idx = 0;
+            for (Map filter : filters) {
+                Iterator<Entry<String, Map>> filterIT = filter.entrySet()
+                        .iterator();
 
-				while (filterIT.hasNext()) {
-					Map.Entry<String, Map> filterEntry = filterIT.next();
-					String filterType = filterEntry.getKey();
-					Map filterConfig = filterEntry.getValue();
+                while (filterIT.hasNext()) {
+                    Map.Entry<String, Map> filterEntry = filterIT.next();
+                    String filterType = filterEntry.getKey();
+                    Map filterConfig = filterEntry.getValue();
 
-					try {
-						Class<?> filterClass = Class
-								.forName("com.ctrip.ops.sysdev.filters."
-										+ filterType);
-						Constructor<?> ctor = filterClass
-								.getConstructor(Map.class);
+                    try {
+                        logger.info("begin to build filter " + filterType);
+                        Class<?> filterClass = Class
+                                .forName("com.ctrip.ops.sysdev.filters."
+                                        + filterType);
+                        Constructor<?> ctor = filterClass
+                                .getConstructor(Map.class);
 
-						BaseFilter filterInstance = (BaseFilter) ctor
-								.newInstance(filterConfig);
-						filterProcessors[idx] = filterInstance;
-					} catch (Exception e) {
-						logger.error(e);
-						System.exit(1);
-					}
-					idx++;
-				}
-			}
-		} else {
-			filterProcessors = null;
-		}
-		return filterProcessors;
-	}
+                        BaseFilter filterInstance = (BaseFilter) ctor
+                                .newInstance(filterConfig);
+                        filterProcessors[idx] = filterInstance;
+                        logger.info("build filter " + filterType + " done");
+                    } catch (Exception e) {
+                        logger.error(e);
+                        System.exit(1);
+                    }
+                    idx++;
+                }
+            }
+        } else {
+            filterProcessors = null;
+        }
+        return filterProcessors;
+    }
 
-	public BaseOutput[] createOutputProcessors() {
-		outputProcessors = new BaseOutput[outputs.size()];
-		int idx = 0;
-		for (Map output : outputs) {
-			Iterator<Entry<String, Map>> outputIT = output.entrySet()
-					.iterator();
+    public BaseOutput[] createOutputProcessors() {
+        outputProcessors = new BaseOutput[outputs.size()];
+        int idx = 0;
+        for (Map output : outputs) {
+            Iterator<Entry<String, Map>> outputIT = output.entrySet()
+                    .iterator();
 
-			while (outputIT.hasNext()) {
-				Map.Entry<String, Map> outputEntry = outputIT.next();
-				String outputType = outputEntry.getKey();
-				Map outputConfig = outputEntry.getValue();
-				Class<?> outputClass;
-				try {
-					outputClass = Class.forName("com.ctrip.ops.sysdev.outputs."
-							+ outputType);
-					Constructor<?> ctor = outputClass.getConstructor(Map.class);
+            while (outputIT.hasNext()) {
+                Map.Entry<String, Map> outputEntry = outputIT.next();
+                String outputType = outputEntry.getKey();
+                Map outputConfig = outputEntry.getValue();
+                Class<?> outputClass;
+                try {
+                    logger.info("begin to build output " + outputType);
+                    outputClass = Class.forName("com.ctrip.ops.sysdev.outputs."
+                            + outputType);
+                    Constructor<?> ctor = outputClass.getConstructor(Map.class);
 
-					outputProcessors[idx] = (BaseOutput) ctor
-							.newInstance(outputConfig);
-					idx++;
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return outputProcessors;
-	}
+                    outputProcessors[idx] = (BaseOutput) ctor
+                            .newInstance(outputConfig);
+                    logger.info("build output " + outputType + " done");
+                    idx++;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return outputProcessors;
+    }
 
-	public IDecode createDecoder() {
-		String codec = (String) this.config.get("codec");
-		if (codec != null && codec.equalsIgnoreCase("plain")) {
-			return new PlainDecoder();
-		} else {
-			return new JsonDecoder();
-		}
-	}
+    public IDecode createDecoder() {
+        String codec = (String) this.config.get("codec");
+        if (codec != null && codec.equalsIgnoreCase("plain")) {
+            return new PlainDecoder();
+        } else {
+            return new JsonDecoder();
+        }
+    }
 
-	public BaseInput(Map config, ArrayList<Map> filters, ArrayList<Map> outputs)
-			throws Exception {
-		this.config = config;
-		this.filters = filters;
-		this.outputs = outputs;
+    public BaseInput(Map config, ArrayList<Map> filters, ArrayList<Map> outputs)
+            throws Exception {
+        this.config = config;
+        this.filters = filters;
+        this.outputs = outputs;
 
-	}
+    }
 
-	protected abstract void prepare();
+    protected abstract void prepare();
 
-	public abstract void emit();
+    public abstract void emit();
 
-	public void process(String message) {
-		Map<String, Object> event = this.decoder.decode(message);
+    public void process(String message) {
+        Map<String, Object> event = this.decoder.decode(message);
 
-		if (this.filterProcessors != null) {
-			for (BaseFilter bf : filterProcessors) {
-				if (event == null) {
-					break;
-				}
-				event = bf.process(event);
-			}
-		}
-		if (event != null) {
-			for (BaseOutput bo : outputProcessors) {
-				bo.process(event);
-			}
-		}
-	};
+        if (this.filterProcessors != null) {
+            for (BaseFilter bf : filterProcessors) {
+                if (event == null) {
+                    break;
+                }
+                event = bf.process(event);
+            }
+        }
+        if (event != null) {
+            for (BaseOutput bo : outputProcessors) {
+                bo.process(event);
+            }
+        }
+    }
+
+    ;
 
 }
