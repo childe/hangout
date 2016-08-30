5e8018b1792abb54b9461a1769930f2dcd731f20
childe <heychilde@gmail.com>
2016-08-30 18:18:34 +0800

    more test case

--- src/test/java/com/ctrip/ops/sysdev/TestGeoIP2.java
+++ src/test/java/com/ctrip/ops/sysdev/TestGeoIP2.java
@@ -1,31 +1,38 @@
 package com.ctrip.ops.sysdev;
 
 import com.ctrip.ops.sysdev.filters.GeoIP2;
-import org.json.simple.JSONValue;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestGeoIP2 {
     @Test
-    public void testGsub() throws UnsupportedEncodingException {
-        String s = "61.240.136.69";
+    public void testGeoIP2() throws UnsupportedEncodingException {
         HashMap config = new HashMap() {
             {
                 put("source", "clientip");
-                put("database", "/Users/liujia/Desktop/es/mydata4vipday2.datx");
+                put("database", "/tmp/GeoLite2-City.mmdb");
             }
         };
 
         GeoIP2 geoip2Filter = new GeoIP2(config);
 
         Map event = new HashMap();
-        event.put("clientip", s);
+        event.put("clientip", "61.240.136.69");
+        event = geoip2Filter.process(event);
+        Assert.assertEquals(((Map)event.get("geoip")).get("country_name"), "China");
+
+        event = new HashMap();
+        event.put("clientip", "10.10.10.10");
         event = geoip2Filter.process(event);
         System.out.println(event);
+        Assert.assertNull(event.get("geoip"));
+        Assert.assertEquals(((ArrayList) event.get("tags")).get(0), "geoipfail");
     }
 }
