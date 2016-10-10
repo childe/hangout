cd2fa366566ab827a3fb3762417778ddcf6a02d7
childe <heychilde@gmail.com>
2016-10-10 11:18:48 +0800

    select which fields are used

--- src/test/java/com/ctrip/ops/sysdev/TestGeoIP2.java
+++ src/test/java/com/ctrip/ops/sysdev/TestGeoIP2.java
@@ -33,12 +33,50 @@ public class TestGeoIP2 {
         event.put("clientip", "61.240.136.69");
         event = geoip2Filter.process(event);
         Assert.assertEquals(((Map) event.get("geoip")).get("country_name"), "China");
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_code"), "CN");
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_isocode"), "CN");
+        Assert.assertNull(((Map) event.get("geoip")).get("subdivision_name"));
+        Assert.assertNull(((Map) event.get("geoip")).get("city_name"));
+
+        event = new HashMap();
+        event.put("clientip", "115.239.211.112");
+        event = geoip2Filter.process(event);
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_name"), "China");
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_code"), "CN");
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_isocode"), "CN");
+        Assert.assertEquals(((Map) event.get("geoip")).get("city_name"), "Hangzhou");
+        Assert.assertEquals(((Map) event.get("geoip")).get("subdivision_name"), "Zhejiang");
+        Assert.assertEquals(((Map) event.get("geoip")).get("longitude"), 120.1614);
+        Assert.assertEquals(((Map) event.get("geoip")).get("latitude"), 30.2936);
 
         event = new HashMap();
         event.put("clientip", "10.10.10.10");
         event = geoip2Filter.process(event);
-        System.out.println(event);
         Assert.assertNull(event.get("geoip"));
         Assert.assertEquals(((ArrayList) event.get("tags")).get(0), "geoipfail");
+
+        config = new HashMap() {
+            {
+                put("source", "clientip");
+                put("database", "/tmp/GeoLite2-City.mmdb");
+                put("country_code", false);
+                put("latitude", false);
+                put("longitude", false);
+            }
+        };
+        geoip2Filter = new GeoIP2(config);
+        event = new HashMap();
+        event.put("clientip", "115.239.211.112");
+        event = geoip2Filter.process(event);
+
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_name"), "China");
+        Assert.assertEquals(((Map) event.get("geoip")).get("country_isocode"), "CN");
+        Assert.assertEquals(((Map) event.get("geoip")).get("city_name"), "Hangzhou");
+        Assert.assertEquals(((Map) event.get("geoip")).get("subdivision_name"), "Zhejiang");
+        Assert.assertNull(((Map) event.get("geoip")).get("country_code"));
+        Assert.assertNull(((Map) event.get("geoip")).get("latitude"));
+        Assert.assertNull(((Map) event.get("geoip")).get("longitude"));
+        Assert.assertEquals(((double[]) ((Map) event.get("geoip")).get("location"))[0], 120.1614);
+        Assert.assertEquals(((double[]) ((Map) event.get("geoip")).get("location"))[1], 30.2936);
     }
 }
