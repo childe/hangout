6cab7d2aa60376ca0e0f8096346a828e9bc2a5b2
childe <heychilde@gmail.com>
2017-01-06 16:21:06 +0800

    get config file from http server

--- hangout-core/src/main/java/com/ctrip/ops/sysdev/config/HangoutConfig.java
+++ hangout-core/src/main/java/com/ctrip/ops/sysdev/config/HangoutConfig.java
@@ -1,20 +1,34 @@
 package com.ctrip.ops.sysdev.config;
 
 
-import org.apache.commons.io.FileUtils;
-import org.yaml.snakeyaml.Yaml;
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
-public class HangoutConfig {
+import org.yaml.snakeyaml.Yaml;
 
-    public static Map parse(File filename) throws IOException {
+@SuppressWarnings("rawtypes")
+public class HangoutConfig {
+    private static final String HTTP = "http://";
+    private static final String HTTPS = "https://";
+//    private static final Logger logger = Logger.getLogger(HangoutConfig.class.getName());
+
+    @SuppressWarnings("NestedMethodCall")
+    public static Map parse(String filename) throws IOException {
         Yaml yaml = new Yaml();
-        String configContent;
-        configContent = FileUtils.readFileToString(filename, "UTF-8");
-        return (Map) yaml.load(configContent);
+        if (filename.startsWith(HangoutConfig.HTTP) || filename.startsWith(HangoutConfig.HTTPS)) {
+            URL httpUrl;
+            URLConnection connection;
+            httpUrl = new URL(filename);
+            connection = httpUrl.openConnection();
+            connection.connect();
+            return (Map) yaml.load(connection.getInputStream());
+        } else {
+            FileInputStream input = new FileInputStream(new File(filename));
+            return (Map) yaml.load(input);
+        }
     }
 }
-
