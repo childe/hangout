62c044aa234b0cf78db847ec70d8a9f6ccade9ba
childe <heychilde@gmail.com>
2017-02-09 11:27:04 +0800

    modified:   hangout-filters/hangout-filters-metric/src/main/java/com/ctrip/ops/sysdev/filters/Metric.java

--- hangout-filters/hangout-filters-metric/src/main/java/com/ctrip/ops/sysdev/filters/Metric.java
+++ hangout-filters/hangout-filters-metric/src/main/java/com/ctrip/ops/sysdev/filters/Metric.java
@@ -1,31 +1,58 @@
 package com.ctrip.ops.sysdev.filters;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.*;
 
 import com.ctrip.ops.sysdev.baseplugin.BaseFilter;
 
 import org.apache.log4j.Logger;
-import com.ctrip.ops.sysdev.render.FreeMarkerRender;
-import com.ctrip.ops.sysdev.render.TemplateRender;
 
-@SuppressWarnings("ALL")
 public class Metric extends BaseFilter {
-	private static final Logger logger = Logger.getLogger(Metric.class.getName());
+    private static final Logger logger = Logger.getLogger(Metric.class.getName());
 
-	public Metric(Map config) {
-		super(config);
-	}
+    private int windowSize;
+    private String key;
+    private String value;
+    private Map<String, Object> metric;
+    private boolean processExtraEventsFunc = true;
 
-	protected void prepare() {
-	}
+    public Metric(Map config) {
+        super(config);
+    }
 
-	@Override
-	protected Map filter(final Map event) {
+    protected void prepare() {
+        this.key = (String) config.get("key");
+        this.value = (String) config.get("value");
+        this.windowSize = (int) config.get("windowSize");
+    }
 
-		return event;
-	}
+    @Override
+    protected Map filter(final Map event) {
+        if (event.containsKey(this.key) && event.containsKey(this.value)) {
+            String keyValue = (String) event.get(this.key);
+            Object valueValue = event.get(this.value);
+            HashMap set = (HashMap) this.metric.get(keyValue);
+            if (set == null) {
+                set = new HashMap();
+                set.put(valueValue, 1);
+            } else {
+                if (set.containsKey(valueValue)) {
+                    int count = (int) set.get(valueValue);
+                    set.put(valueValue, count + 1);
+                } else {
+                    set.put(valueValue, 1);
+                }
+            }
+            this.metric.put(keyValue, set);
+        }
+
+        return event;
+    }
+
+    @Override
+    public List<Map<String, Object>> filterExtraEvents(Map event) {
+        List<Map<String, Object>> events = new ArrayList<Map<String, Object>>();
+        events.add(event);
+        events.add(this.metric);
+        return events;
+    }
 }
