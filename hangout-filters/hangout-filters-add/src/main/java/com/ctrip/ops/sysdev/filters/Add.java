1dfe408d273ee745a8ba3470a9c580690006395f
childe <heychilde@gmail.com>
2017-02-10 22:18:30 +0800

    use new template

--- hangout-filters/hangout-filters-add/src/main/java/com/ctrip/ops/sysdev/filters/Add.java
+++ hangout-filters/hangout-filters-add/src/main/java/com/ctrip/ops/sysdev/filters/Add.java
@@ -14,44 +14,44 @@ import com.ctrip.ops.sysdev.render.TemplateRender;
 
 @SuppressWarnings("ALL")
 public class Add extends BaseFilter {
-	private static final Logger logger = Logger.getLogger(Add.class.getName());
+    private static final Logger logger = Logger.getLogger(Add.class.getName());
 
-	public Add(Map config) {
-		super(config);
-	}
+    public Add(Map config) {
+        super(config);
+    }
 
-	private Map<String, TemplateRender> f;
+    private Map<String, TemplateRender> f;
 
-	protected void prepare() {
-		f = new HashMap<String, TemplateRender>();
+    protected void prepare() {
+        f = new HashMap<String, TemplateRender>();
 
-		Map<String, String> fields = (Map<String, String>) config.get("fields");
-		Iterator<Entry<String, String>> it = fields.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<String, String> entry = it.next();
+        Map<String, String> fields = (Map<String, String>) config.get("fields");
+        Iterator<Entry<String, String>> it = fields.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> entry = it.next();
 
-			String field = entry.getKey();
-			String value = entry.getValue();
+            String field = entry.getKey();
+            String value = entry.getValue();
 
-			try {
-				this.f.put(field, new FreeMarkerRender(value, value));
-			} catch (IOException e) {
-				logger.fatal(e.getMessage());
-				System.exit(1);
-			}
-		}
-	}
+            try {
+                this.f.put(field, TemplateRender.getRender(value));
+            } catch (IOException e) {
+                logger.fatal(e.getMessage());
+                System.exit(1);
+            }
+        }
+    }
 
-	@Override
-	protected Map filter(final Map event) {
-		Iterator<Entry<String, TemplateRender>> it = f.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<String, TemplateRender> entry = it.next();
+    @Override
+    protected Map filter(final Map event) {
+        Iterator<Entry<String, TemplateRender>> it = f.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, TemplateRender> entry = it.next();
 
-			String field = entry.getKey();
-			TemplateRender render = entry.getValue();
-			event.put(field, render.render(event));
-		}
-		return event;
-	}
+            String field = entry.getKey();
+            TemplateRender render = entry.getValue();
+            event.put(field, render.render(event));
+        }
+        return event;
+    }
 }
