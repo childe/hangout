85ab8bafd1913b5070d97a507bf5b06eac1208d9
childe <heychilde@gmail.com>
2017-02-13 11:42:54 +0800

    extends metric and add test case

--- hangout-filters/hangout-filters-flatmetric/src/main/java/com/ctrip/ops/sysdev/filters/FlatMetric.java
+++ hangout-filters/hangout-filters-flatmetric/src/main/java/com/ctrip/ops/sysdev/filters/FlatMetric.java
@@ -2,65 +2,27 @@ package com.ctrip.ops.sysdev.filters;
 
 import java.util.*;
 
-import com.ctrip.ops.sysdev.baseplugin.BaseFilter;
 
 import org.apache.log4j.Logger;
 
-public class FlatMetric extends BaseFilter {
+public class FlatMetric extends Metric {
     private static final Logger logger = Logger.getLogger(FlatMetric.class.getName());
-
-    private int windowSize;
-    private String key;
-    private String value;
-    private Map<Object, Map<Object, Integer>> metric;
-    private long lastEmitTime;
 
     public FlatMetric(Map config) {
         super(config);
-    }
-
-    protected void prepare() {
-        this.key = (String) config.get("key");
-        this.value = (String) config.get("value");
-        this.windowSize = (int) config.get("windowSize") * 1000;
-        this.processExtraEventsFunc = true;
-        this.metric = new HashMap();
-        this.lastEmitTime = System.currentTimeMillis();
-    }
-
-    @Override
-    protected Map filter(final Map event) {
-        if (event.containsKey(this.key) && event.containsKey(this.value)) {
-            Object keyValue = event.get(this.key);
-            Object valueValue = event.get(this.value);
-            HashMap set = (HashMap) this.metric.get(keyValue);
-            if (set == null) {
-                set = new HashMap();
-                set.put(valueValue, 1);
-            } else {
-                if (set.containsKey(valueValue)) {
-                    int count = (int) set.get(valueValue);
-                    set.put(valueValue, count + 1);
-                } else {
-                    set.put(valueValue, 1);
-                }
-            }
-            this.metric.put(keyValue, set);
-        }
-
-        return event;
+        this.metricToEmit = new HashMap<>();
     }
 
     @Override
     public List<Map<String, Object>> filterExtraEvents(Map event) {
-        if (System.currentTimeMillis() < this.windowSize + this.lastEmitTime) {
+        if (metricToEmit.size() == 0) {
             return null;
         }
         List<Map<String, Object>> events = new ArrayList<Map<String, Object>>();
-        Iterator<Map.Entry<Object, Map<Object, Integer>>> it = this.metric.entrySet().iterator();
-        this.lastEmitTime = System.currentTimeMillis();
+        Iterator<Map.Entry<String, Object>> it = this.metricToEmit.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry<Object, Map<Object, Integer>> o = it.next();
+            Map.Entry<String, Object> o = it.next();
+//            Map.Entry<String, Map<Object, Integer>> o = it.next();
             final Object keyValue = o.getKey();
             final Map ValueValue = (Map) o.getValue();
             Iterator<Map.Entry<Object, Integer>> vvit = ValueValue.entrySet().iterator();
@@ -77,7 +39,8 @@ public class FlatMetric extends BaseFilter {
             }
 
         }
-        this.metric = new HashMap();
+        this.metricToEmit.clear();
+        this.lastEmitTime = System.currentTimeMillis();
 
         return events;
     }
