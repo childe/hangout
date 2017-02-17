5e0699755c2e3c9d4bd37f58f2803179224ac4c0
gnuhpc <gnuhpc@gmail.com>
2017-02-17 18:14:00 +0800

    add admin http server

--- hangout-output-plugins/hangout-output-plugins-stdout/src/main/java/com/ctrip/ops/sysdev/outputs/Stdout.java
+++ hangout-output-plugins/hangout-output-plugins-stdout/src/main/java/com/ctrip/ops/sysdev/outputs/Stdout.java
@@ -2,6 +2,7 @@ package com.ctrip.ops.sysdev.outputs;
 
 import java.util.Map;
 
+import com.codahale.metrics.Meter;
 import com.ctrip.ops.sysdev.baseplugin.BaseOutput;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
@@ -10,17 +11,20 @@ import org.apache.log4j.Logger;
 @Log4j
 public class Stdout extends BaseOutput {
 
-	public Stdout(Map config) {
-		super(config);
-	}
+    private Meter meter;
+    public Stdout(Map config) {
+        super(config);
+    }
 
-	@Override
-	protected void prepare() {
+    @Override
+    protected void prepare() {
+        this.meter = watcher.setMetric(this.getClass().getSimpleName());
+    }
 
-	}
-
-	@Override
-	protected void emit(Map event) {
-		System.out.println(event);
-	}
+    @Override
+    protected void emit(Map event) {
+        System.out.println(event);
+        this.meter.mark();
+    }
 }
+
