7f215b9cfdd5aadffd628addbe51d6cd1b431036
childe <heychilde@gmail.com>
2017-02-26 00:09:08 +0800

    setfield and render field

--- hangout-filters/hangout-filters-convert/src/test/java/com/ctrip/ops/sysdev/test/TestConvertFilter.java
+++ hangout-filters/hangout-filters-convert/src/test/java/com/ctrip/ops/sysdev/test/TestConvertFilter.java
@@ -11,85 +11,107 @@ import org.testng.annotations.Test;
 import org.yaml.snakeyaml.Yaml;
 
 public class TestConvertFilter {
-	@Test
-	@SuppressWarnings({ "rawtypes", "unchecked", "serial" })
-	public void testDateFilter() throws UnsupportedEncodingException {
-		String c = String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s",
-				"        fields:", "            cs_bytes: ",
-				"                to: integer",
-				"                remove_if_fail: true",
-				"            time_taken: ", "                to: float",
-				"                setto_if_fail: 0.0");
-		Yaml yaml = new Yaml();
-		Map config = (Map) yaml.load(c);
-		Assert.assertNotNull(config);
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked", "serial"})
+    public void testConvertFilter() throws UnsupportedEncodingException {
+        String c = String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s",
+                "        fields:",
+                "            cs_bytes: ",
+                "                to: integer",
+                "                remove_if_fail: true",
+                "            time_taken: ",
+                "                to: float",
+                "                setto_if_fail: 0.0");
+        Yaml yaml = new Yaml();
+        Map config = (Map) yaml.load(c);
+        Assert.assertNotNull(config);
 
-		Convert convertfilter = new Convert(config);
+        Convert convertFilter = new Convert(config);
 
-		// Match
-		Map event = new HashMap();
-		event.put("cs_bytes", "12");
-		event.put("time_taken", "11.11");
+        // Match
+        Map event = new HashMap();
+        event.put("cs_bytes", "12");
+        event.put("time_taken", "11.11");
 
-		event = convertfilter.process(event);
-		Assert.assertEquals(event.get("cs_bytes"), 12);
-		Assert.assertEquals(event.get("time_taken"), 11.11F);
-		Assert.assertNull(event.get("tags"));
+        event = convertFilter.process(event);
+        Assert.assertEquals(event.get("cs_bytes"), 12);
+        Assert.assertEquals(event.get("time_taken"), 11.11F);
+        Assert.assertNull(event.get("tags"));
 
-		// remove_if_fail
-		event = new HashMap();
-		event.put("cs_bytes", "12.2");
+        // remove_if_fail
+        event = new HashMap();
+        event.put("cs_bytes", "12.2");
 
-		event = convertfilter.process(event);
-		Assert.assertNull(event.get("cs_bytes"));
-		Assert.assertEquals(((ArrayList) event.get("tags")).get(0),
-				"convertfail");
+        event = convertFilter.process(event);
+        Assert.assertNull(event.get("cs_bytes"));
+        Assert.assertEquals(((ArrayList) event.get("tags")).get(0),
+                "convertfail");
 
-		// setto_if_fail
-		event = new HashMap();
-		event.put("time_taken", "12.2a");
+        // setto_if_fail
+        event = new HashMap();
+        event.put("time_taken", "12.2a");
 
-		event = convertfilter.process(event);
-		Assert.assertEquals(event.get("time_taken"), 0.0);
-		Assert.assertEquals(((ArrayList) event.get("tags")).get(0),
-				"convertfail");
+        event = convertFilter.process(event);
+        Assert.assertEquals(event.get("time_taken"), 0.0);
+        Assert.assertEquals(((ArrayList) event.get("tags")).get(0),
+                "convertfail");
 
-		// set tag
-		c = String.format("%s\n%s\n%s\n%s\n%s\n%s",
-				"fields:",
-				"  cs_bytes: ",
-				"    to: integer",
-				"    remove_if_fail: true",
-				"  time_taken: ",
-				"    to: float");
-		yaml = new Yaml();
-		config = (Map) yaml.load(c);
+        // set tag
+        c = String.format("%s\n%s\n%s\n%s\n%s\n%s",
+                "fields:",
+                "  cs_bytes: ",
+                "    to: integer",
+                "    remove_if_fail: true",
+                "  time_taken: ",
+                "    to: float");
+        yaml = new Yaml();
+        config = (Map) yaml.load(c);
 
-		convertfilter = new Convert(config);
-		event = new HashMap();
-		event.put("time_taken", "12.2a");
+        convertFilter = new Convert(config);
+        event = new HashMap();
+        event.put("time_taken", "12.2a");
 
-		event = convertfilter.process(event);
-		Assert.assertEquals(((ArrayList) event.get("tags")).get(0),
-				"convertfail");
+        event = convertFilter.process(event);
+        Assert.assertEquals(((ArrayList) event.get("tags")).get(0),
+                "convertfail");
 
-		// NOT set tag
-		c = String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s",
-				"tag_on_failure: null",
-				"fields:",
-				"  cs_bytes: ",
-				"    to: integer",
-				"    remove_if_fail: true",
-				"  time_taken: ",
-				"    to: float");
-		yaml = new Yaml();
-		config = (Map) yaml.load(c);
+        // NOT set tag
+        c = String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s",
+                "tag_on_failure: null",
+                "fields:",
+                "  cs_bytes: ",
+                "    to: integer",
+                "    remove_if_fail: true",
+                "  time_taken: ",
+                "    to: float");
+        yaml = new Yaml();
+        config = (Map) yaml.load(c);
 
-		convertfilter = new Convert(config);
-		event = new HashMap();
-		event.put("time_taken", "12.2a");
+        convertFilter = new Convert(config);
+        event = new HashMap();
+        event.put("time_taken", "12.2a");
 
-		event = convertfilter.process(event);
-		Assert.assertNull((ArrayList) event.get("tags"));
-	}
+        event = convertFilter.process(event);
+        Assert.assertNull((ArrayList) event.get("tags"));
+
+        // test multi level field
+        c = String.format("%s\n%s\n%s\n%s\n%s",
+                "tag_on_failure: null",
+                "fields:",
+                "  '[metric][a]': ",
+                "    to: integer",
+                "    remove_if_fail: true");
+        yaml = new Yaml();
+        config = (Map) yaml.load(c);
+
+        convertFilter = new Convert(config);
+        event = new HashMap();
+        event.put("metric", new HashMap() {{
+            this.put("a", "1200");
+        }});
+
+        event = convertFilter.process(event);
+        Assert.assertNull((ArrayList) event.get("tags"));
+        Assert.assertEquals(1200, ((Map) event.get("metric")).get("a"));
+    }
 }
