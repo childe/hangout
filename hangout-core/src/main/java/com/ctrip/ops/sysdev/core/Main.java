5785eaddbc7052f9956c1ff65cb1615700535c31
childe <heychilde@gmail.com>
2017-07-04 15:50:44 +0800

    add metrics plugin

--- hangout-core/src/main/java/com/ctrip/ops/sysdev/core/Main.java
+++ hangout-core/src/main/java/com/ctrip/ops/sysdev/core/Main.java
@@ -4,6 +4,7 @@ import com.codahale.metrics.servlets.AdminServlet;
 import com.codahale.metrics.servlets.HealthCheckServlet;
 import com.codahale.metrics.servlets.MetricsServlet;
 import com.ctrip.ops.sysdev.baseplugin.BaseInput;
+import com.ctrip.ops.sysdev.baseplugin.BaseMetric;
 import com.ctrip.ops.sysdev.config.CommandLineValues;
 import com.ctrip.ops.sysdev.config.HangoutConfig;
 import com.ctrip.ops.sysdev.log.LogSetter;
@@ -14,17 +15,16 @@ import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
-import lombok.extern.log4j.Log4j;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
-@Log4j
 public class Main {
     private static LogSetter logSetter = new LogSetter();
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
@@ -43,20 +43,58 @@ public class Main {
             e.printStackTrace();
             System.exit(-1);
         }
-        log.debug(configs);
+        logger.debug(configs);
 
         final List<HashMap<String, Map>> inputConfigs = (ArrayList<HashMap<String, Map>>) configs.get("inputs");
         final List<HashMap<String, Map>> filterConfigs = (ArrayList<HashMap<String, Map>>) configs.get("filters");
         final List<HashMap<String, Map>> outputConfigs = (ArrayList<HashMap<String, Map>>) configs.get("outputs");
-        final HashMap<String, Object> commonsConfigs = (HashMap<String, Object>) configs.get("commons");
+        final List<HashMap<String, Map>> metricsConfigs = (ArrayList<HashMap<String, Map>>) configs.get("metrics");
 
+        //TODO remove this and put it to metrics
+        final HashMap<String, Object> commonsConfigs = (HashMap<String, Object>) configs.get("commons");
         runAdminServer(commonsConfigs);
+
+        metricsConfigs.forEach(metric -> {
+            metric.forEach((metricType, metricConfig) -> {
+                logger.info("begin to build metric " + metricType);
+
+                Class<?> metricClass = null;
+
+                List<String> classNames = Arrays.asList("com.ctrip.ops.sysdev.metrics." + metricType, metricType);
+                boolean tryCtrip = true;
+                for (String className : classNames) {
+                    try {
+                        metricClass = Class.forName(className);
+                        Constructor<?> ctor = metricClass.getConstructor(Map.class);
+                        BaseMetric metricInstance = (BaseMetric) ctor.newInstance(metricConfig);
+                        logger.info("build metric " + metricType + " done");
+
+                        metricInstance.register();
+                        logger.info("metric" + metricType + " started");
+
+                        break;
+                    } catch (ClassNotFoundException e) {
+                        if (tryCtrip == true) {
+                            logger.info("maybe a third party metric plugin. try to build " + metricType);
+                            tryCtrip = false;
+                            continue;
+                        } else {
+                            logger.error(e);
+                            System.exit(-1);
+                        }
+                    } catch (Exception e) {
+                        logger.error(e);
+                        System.exit(-1);
+                    }
+                }
+            });
+        });
 
         // for input in all_inputs, Go through every input and emit immediately
         inputConfigs.forEach(
                 input -> {
                     input.forEach((inputType, inputConfig) -> {
-                        log.info("begin to build input " + inputType);
+                        logger.info("begin to build input " + inputType);
 
                         Class<?> inputClass = null;
 
@@ -76,22 +114,22 @@ public class Main {
                                         filterConfigs,
                                         outputConfigs);
 
-                                log.info("build input " + inputType + " done");
+                                logger.info("build input " + inputType + " done");
                                 //Start working,guy.
                                 inputInstance.emit();
-                                log.info("input" + inputType + " started");
+                                logger.info("input" + inputType + " started");
                                 break;
                             } catch (ClassNotFoundException e) {
                                 if (tryCtrip == true) {
-                                    log.info("maybe a third party input plugin. try to build " + inputType);
+                                    logger.info("maybe a third party input plugin. try to build " + inputType);
                                     tryCtrip = false;
                                     continue;
                                 } else {
-                                    log.error(e);
+                                    logger.error(e);
                                     System.exit(-1);
                                 }
                             } catch (Exception e) {
-                                log.error(e);
+                                logger.error(e);
                                 System.exit(-1);
                             }
                         }
@@ -128,7 +166,7 @@ public class Main {
                 try {
                     path = Handlers.path().addPrefixPath("/", manager.start());
                 } catch (ServletException e) {
-                    log.error("Admin Servlet error: ", e);
+                    logger.error("Admin Servlet error: ", e);
                 }
 
                 Undertow server = Undertow.builder()
@@ -138,7 +176,7 @@ public class Main {
                 try {
                     server.start();
                 } catch (RuntimeException e) {
-                    log.error("Admin server start error: ", e);
+                    logger.error("Admin server start error: ", e);
                 }
 
             };
