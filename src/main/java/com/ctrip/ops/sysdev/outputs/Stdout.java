e63f7d35909abb75592eac599b1e67e4553a90ee
yanggenxing <yanggenxing@e.hunantv.com>
2017-08-21 19:31:26 +0800

    add json output when stdout

--- src/main/java/com/ctrip/ops/sysdev/outputs/Stdout.java
+++ src/main/java/com/ctrip/ops/sysdev/outputs/Stdout.java
@@ -10,18 +10,27 @@ public class Stdout extends BaseOutput {
 	private static final Logger logger = Logger.getLogger(Stdout.class
 			.getName());
 
+	private String format;
 	public Stdout(Map config) {
 		super(config);
 	}
 
 	@Override
 	protected void prepare() {
+		if (config.containsKey("format")) {
+			this.format = (String) config.get("format");
+		} else {
+			this.format = "plain";
+		}
 
 	}
 
 	@Override
 	protected void emit(Map event) {
-		// System.out.println(event);
-		System.out.println(JSONValue.toJSONString(event));
+		if (this.format.equals("json")) {
+			System.out.println(JSONValue.toJSONString(event));
+		} else {
+		 	System.out.println(event);
+		}
 	}
 }
