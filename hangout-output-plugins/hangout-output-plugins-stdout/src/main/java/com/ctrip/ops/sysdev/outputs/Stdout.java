a6443910aec664115a59c8a8c5552f6d54cf7199
childe <heychilde@gmail.com>
2018-01-16 22:42:05 +0800

    format render

--- hangout-output-plugins/hangout-output-plugins-stdout/src/main/java/com/ctrip/ops/sysdev/outputs/Stdout.java
+++ hangout-output-plugins/hangout-output-plugins-stdout/src/main/java/com/ctrip/ops/sysdev/outputs/Stdout.java
@@ -1,12 +1,17 @@
 package com.ctrip.ops.sysdev.outputs;
 
+import java.io.IOException;
 import java.util.Map;
 
 import com.ctrip.ops.sysdev.baseplugin.BaseOutput;
+import com.ctrip.ops.sysdev.render.TemplateRender;
 import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONValue;
 
 @Log4j2
 public class Stdout extends BaseOutput {
+
+    private TemplateRender format;
 
     public Stdout(Map config) {
         super(config);
@@ -14,11 +19,30 @@ public class Stdout extends BaseOutput {
 
     @Override
     protected void prepare() {
+        if (this.config.containsKey("format")) {
+            String format = (String) this.config.get("format");
+            try {
+                this.format = TemplateRender.getRender(format);
+            } catch (IOException e) {
+                log.fatal("could not build template from" + format);
+                System.exit(1);
+            }
+        } else {
+            this.format = null;
+        }
     }
 
     @Override
     protected void emit(Map event) {
-        System.out.println(event);
+        if (this.format == null) {
+            System.out.println(event);
+        } else {
+            Object message = this.format.render(event);
+            if (message != null) {
+                System.out.println(message);
+            }
+        }
+
     }
 }
 
