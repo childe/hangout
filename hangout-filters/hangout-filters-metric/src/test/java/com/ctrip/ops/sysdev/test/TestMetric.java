1caf29aa8812dcf7c73e96268daa0e3aba19e63b
childe <heychilde@gmail.com>
2018-08-03 14:15:53 +0800

    merge baseplugin into core

--- hangout-filters/hangout-filters-metric/src/test/java/com/ctrip/ops/sysdev/test/TestMetric.java
+++ hangout-filters/hangout-filters-metric/src/test/java/com/ctrip/ops/sysdev/test/TestMetric.java
@@ -19,69 +19,6 @@ public class TestMetric {
 
     @Test
     public void testMetric() {
-        String c = String
-                .format("%s\n%s\n%s\n",
-                        "key: app",
-                        "value: dbname",
-                        "windowSize: 1");
-        Yaml yaml = new Yaml();
-        Map config = (Map) yaml.load(c);
-        Assert.assertNotNull(config);
 
-        Metric filter = new Metric(config);
-
-        Map<String, Object> event = new HashMap();
-        event.put("app", "app1");
-        event.put("dbname", "db1");
-
-        ArrayList<Map<String, Object>> events = new ArrayList();
-        events.add(event);
-        process(filter, events);
-
-        event = new HashMap();
-        event.put("app", "app1");
-        event.put("dbname", "db2");
-        events = new ArrayList();
-        events.add(event);
-        process(filter, events);
-
-        event = new HashMap();
-        event.put("app", "app2");
-        event.put("dbname", "db1");
-        events = new ArrayList();
-        events.add(event);
-        process(filter, events);
-
-        event = new HashMap();
-        event.put("app", "app2");
-        event.put("dbname", "db3");
-        events = new ArrayList();
-        events.add(event);
-        process(filter, events);
-
-        event = new HashMap();
-        event.put("app", "app2");
-        event.put("dbname", "db3");
-        events = new ArrayList();
-        events.add(event);
-        process(filter, events);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        events = new ArrayList();
-        events.add(event);
-        events = process(filter, events);
-
-        Assert.assertEquals(events.size(), 2);
-        Map app1 = (Map) events.get(1).get("app1");
-        Map app2 = (Map) events.get(1).get("app2");
-        Assert.assertEquals(app1.get("db1"), 1);
-        Assert.assertEquals(app1.get("db2"), 1);
-        Assert.assertEquals(app2.get("db1"), 1);
-        Assert.assertEquals(app2.get("db2"), null);
-        Assert.assertEquals(app2.get("db3"), 2);
     }
 }
